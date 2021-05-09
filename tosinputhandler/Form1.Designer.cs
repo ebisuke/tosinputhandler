cade489133a787fa6a632c544aeb0909a1b22cf0
ebisuke <fungi@rect.sakura.ne.jp>
2021-05-09 21:07:47 +0900

    wip

--- tosinputhandler/Form1.Designer.cs
+++ tosinputhandler/Form1.Designer.cs
@@ -24,16 +24,61 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(160, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(200, 20);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textBox1;
 
         #endregion
     }
--- tosinputhandler/Form1.cs
+++ tosinputhandler/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,50 @@ namespace tosinputhandler
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveInputText();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SaveInputText()
+        {
+            using (StreamWriter tx = new StreamWriter(Path.Combine(  Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName),"tx.txt") ))
+            {
+                tx.Write(textBox1.Text);
+            }
+            this.Close();
+        }
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var args=Environment.GetCommandLineArgs();
+            if (args.Length >= 5)
+            {
+                Rectangle rect = new Rectangle(int.Parse(args[1]), int.Parse(args[2]), int.Parse(args[3]),
+                    int.Parse(args[4]));
+                Bounds = rect;
+            }   
+            using (StreamReader rx = new StreamReader(Path.Combine(  Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName),"rx.txt") ))
+            {
+                textBox1.Text= rx.ReadToEnd();
+            }
         }
     }
 }
